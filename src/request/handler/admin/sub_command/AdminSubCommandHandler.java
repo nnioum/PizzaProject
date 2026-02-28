59b1c7c28ea68c6a817a17d6419832e559b1e2bc
Serafima <simapono.4@gmail.com>
2026-02-28 22:28:01 +0700

    реализация обработчика для ингредиентов

--- src/request/handler/admin/sub_command/AdminSubCommandHandler.java
+++ src/request/handler/admin/sub_command/AdminSubCommandHandler.java
@@ -1,5 +1,19 @@
 package request.handler.admin.sub_command;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public abstract class AdminSubCommandHandler {
+    protected final Map<String, ParamsSpec> paramsSpecByCommand = new HashMap<>();
+
     public abstract void handle(String... subWord);
+
+    protected Map<String, String> parseParams(String... subLineBlocks) {
+        return Arrays.stream(subLineBlocks)
+                .map(s -> s.split("="))
+                .collect(Collectors.toMap(a -> a[0], a -> a[1]));
+    }
 }
--- src/request/handler/admin/sub_command/handler/IngredientCommandHandler.java
+++ src/request/handler/admin/sub_command/handler/IngredientCommandHandler.java
@@ -1,21 +1,32 @@
 package request.handler.admin.sub_command.handler;
 
+import controller.IngredientController;
 import request.handler.admin.sub_command.AdminSubCommandHandler;
+import request.handler.admin.sub_command.ParamsSpec;
 
 import java.util.*;
 
 public class IngredientCommandHandler extends AdminSubCommandHandler {
-    private final Set<String> createRequiredParams = Set.of("--name", "--price");
-    private final Set<String> createOptionalParams = Set.of();
+    private final IngredientController ingredientController = new IngredientController();
 
-    private final Set<String> editRequiredParams = Set.of("--name");
-    private final Set<String> editOptionalParams = Set.of("--new-name", "--price");
+    public IngredientCommandHandler() {
+        ParamsSpec createParamsSpec = new ParamsSpec();
+        createParamsSpec.addRequiredParams(List.of("--name", "--price"));
+        paramsSpecByCommand.put("create", createParamsSpec);
 
-    private final Set<String> deleteRequiredParams = Set.of("--name");
-    private final Set<String> deleteOptionalParams = Set.of();
+        ParamsSpec editParamsSpec = new ParamsSpec();
+        editParamsSpec.addRequiredParams(List.of("--name"));
+        editParamsSpec.addOptionalParams(List.of("--new-name", "--price"));
+        paramsSpecByCommand.put("edit", editParamsSpec);
 
-    private final Set<String> listRequiredParams = Set.of();
-    private final Set<String> listOptionalParams = Set.of();
+        ParamsSpec deleteParamsSpec = new ParamsSpec();
+        deleteParamsSpec.addRequiredParams(List.of("--name"));
+        paramsSpecByCommand.put("delete", deleteParamsSpec);
+
+        ParamsSpec getParamsSpec = new ParamsSpec();
+        getParamsSpec.addRequiredParams(List.of("--name"));
+        paramsSpecByCommand.put("get", getParamsSpec);
+    }
 
     @Override
     public void handle(String... subWords) {//добавить s
@@ -24,22 +35,21 @@ public class IngredientCommandHandler extends AdminSubCommandHandler {
             return;
         }
         String resourcesCommand = subWords[0];
-        String[] words = Arrays.copyOfRange(subWords, 1, subWords.length);
-        Map<String, String> commands = new HashMap<>();
-        for (String command : words) {
-            String[] blocks = command.split("=");
-            String key = blocks[0], value = blocks[1];
-            commands.put(key, value);
-        }
+        String[] paramsBlocks = Arrays.copyOfRange(subWords, 1, subWords.length);
+        Map<String, String> params = parseParams(paramsBlocks);
+        ParamsSpec paramsSpec = paramsSpecByCommand.get(resourcesCommand);
 
         switch (resourcesCommand) {
             case "create":
-                String name = null, price = null;
-                for (String requiredKey : createRequiredParams) {
-                    if (commands.containsKey(requiredKey)) {
-
+                Set<String> requiredParams = paramsSpec.getRequiredParams();
+                for (String param : requiredParams) {
+                    if (!params.containsKey(param)) {
+                        System.out.println("Параметр " + param + " обязательный");
+                        return;
                     }
                 }
+//                ingredientController.create();
+
         }
     }
 }
